46aafe3a2754cac0722600e68f7ee654d5f648e9
Atul Kumar <iatulkumar35076@gmail.com>
2026-03-01 02:09:15 +0530

    Add twoSum function for finding two indices
    Implement twoSum function using hash map for efficient lookup.

---
+++ Arrays/two_sum.cpp
@@ -0,0 +1,19 @@
+// Hash Map approach
+// TC: O(n)
+// SC: O(n)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int,int> mp;
+
+    for(int i = 0; i < nums.size(); i++) {
+        int diff = target - nums[i];
+        if(mp.find(diff) != mp.end()) {
+            return {mp[diff], i};
+        }
+        mp[nums[i]] = i;
+    }
+    return {};
+}
